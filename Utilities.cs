882ee6ed5070e3c3247c671dcd42aa376793394f
Santhosh <santhosh@tillidsoft.com>
2013-02-19 16:09:37 +0530

    modified key

--- Utilities.cs
+++ Utilities.cs
@@ -5,7 +5,7 @@ namespace AuthorizeSample
     {
         public static string SiteUrl = "http://authorizesample.apphb.com";
         public static string ApiLoginID = "8hw54A7VJ";
-        public static string TransactionKey = "7443fLVdSp89dU321";
+        public static string TransactionKey = "7443fLVdSp89dU32";
         public static string MerchantHash = "C2D03ADC1E90F1221BA9";
     }
 }
