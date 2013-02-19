4cf5b861920c4cba10d3f1256d570487ca2c231d
unknown <Santhosh@TILLID-PC12.tillid.com>
2013-02-19 12:59:16 +0530

    modified url

--- Utilities.cs
+++ Utilities.cs
@@ -3,7 +3,7 @@ namespace AuthorizeSample
 {
     public class Utilities
     {
-        public static string SiteUrl = "http://localhost:42572";
+        public static string SiteUrl = "http://authorizesample.apphb.com";
         public static string ApiLoginID = "8hw54A7VJ";
         public static string TransactionKey = "7443fLVdSp89dU32";
     }
