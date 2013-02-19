aa23002cbc3730c2b0d6dcf189064618fc1fbe9f
unknown <Santhosh@TILLID-PC12.tillid.com>
2013-02-19 12:51:59 +0530

    kick off to git

---
+++ Controllers/AuthorizeController.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Mvc;
+
+namespace AuthorizeSample.Controllers
+{
+    public class AuthorizeController : Controller
+    {
+        //
+        // GET: /Authorize/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Status()
+        {
+            ViewBag.Status = Request.QueryString["m"] ?? "";
+            return View();
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Sim(FormCollection post)
+        {
+            var response = new AuthorizeNet.SIMResponse(post);
+            var isValid = response.Validate(Utilities.TransactionKey, Utilities.ApiLoginID);
+            if (!isValid)
+                return Redirect("/");
+            var returnUrl = Utilities.SiteUrl + "/Authorize/Status?m=" + response.Message;
+            return Content(AuthorizeNet.Helpers.CheckoutFormBuilders.Redirecter(returnUrl));
+
+        }
+
+    }
+}
---
+++ Utilities.cs
@@ -0,0 +1,10 @@
+﻿
+namespace AuthorizeSample
+{
+    public class Utilities
+    {
+        public static string SiteUrl = "http://localhost:42572";
+        public static string ApiLoginID = "8hw54A7VJ";
+        public static string TransactionKey = "7443fLVdSp89dU32";
+    }
+}
